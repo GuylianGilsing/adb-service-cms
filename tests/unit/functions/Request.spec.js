c7589e7bca1c6b7701a8f8915004576cba02d6ba
Gilsing,Guylian Gilsing G <g.gilsing@student.fontys.nl>
2022-01-06 13:53:17 +0100

    Fixed static code analysis errors

--- tests/unit/functions/Request.spec.js
+++ tests/unit/functions/Request.spec.js
@@ -3,7 +3,7 @@ import request from "../../../src/functions/Request";
 test('That the request wrapper only returns the response', async () => {
     // Arrange
     const responseData = 'success';
-    const returnsResponsePromise = () => new Promise((resolve, reject) => {
+    const returnsResponsePromise = () => new Promise((resolve) => {
         resolve(responseData);
     });
 
